62d214ffbbd0233d1fa7942aa5daae2d16373eea
Your Name <kevin.kds80@gmail.com>
2021-09-24 23:07:47 +0000

    Add file to test stages of compiler

---
+++ Src/Compiler/TestFeatures.hs
@@ -0,0 +1,14 @@
+module Lang.Src.Compiler.TestFeatures where
+
+import Lang.Src.Compiler.LexicalAnalyser
+
+tokens = do
+    let program = "fn fnc <: _v=0 v1=12 :> v = 90 export 90" ---"fnw3030fn ,=..."
+    --startLexicalAnalysis program 1 0
+    getAllTokensFromProgram program 1 0
+
+getAllTokensFromProgram [] _ _= []
+getAllTokensFromProgram xs l c = 
+    parsedToken:getAllTokensFromProgram remain line col 
+    where
+        Right (parsedToken, line, col, remain) = startLexicalAnalysis xs l c
