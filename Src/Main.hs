3edc5226ef30e1b431389affa841c3ab74a58b19
Your Name <kevin.kds80@gmail.com>
2021-09-24 23:07:02 +0000

    Add base source file

---
+++ Src/Main.hs
@@ -0,0 +1,17 @@
+module Main where
+
+main :: IO ()
+main = do
+  putStrLn "hello world"
+
+{- 
+fn fnc <: _v=0 v1=12 :>
+   if _v > 45 :
+     show "greater"
+   ;
+export 90
+
+fn main <::>
+  let = fnc 12,12.
+  show let
+export 0 -}
