8ae8253050f9d9ce95883dbb570c919ea38ea58e
Your Name <kevin.kds80@gmail.com>
2021-09-25 23:48:26 +0000

    Add sintatic analysis function test

--- Src/Compiler/TestFeatures.hs
+++ Src/Compiler/TestFeatures.hs
@@ -1,6 +1,7 @@
 module Lang.Src.Compiler.TestFeatures where
 
 import Lang.Src.Compiler.LexicalAnalyser
+import Lang.Src.Compiler.SintaticAnalyser
 
 tokens = do
     let program = "fn fnc <: _v=0 v1=12 :> v = 90 export 90" ---"fnw3030fn ,=..."
@@ -12,3 +13,7 @@ getAllTokensFromProgram xs l c =
     parsedToken:getAllTokensFromProgram remain line col 
     where
         (parsedToken, line, col, remain) = startLexicalAnalysis xs l c
+
+sintaticAnalysis = do
+    let program = "r = 22 fn fnc <: _v=0 v1=12 :> v = 90 export 90"
+    startSintaticAnalysis program 1 0 0 0
