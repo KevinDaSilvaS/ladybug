de744fb2bf5cd7d8ef522a41a0fcaf80d42331fc
Your Name <kevin.kds80@gmail.com>
2021-09-28 23:15:46 +0000

    Add parseTree construction

--- Src/Compiler/ParseTree.hs
+++ Src/Compiler/ParseTree.hs
@@ -4,42 +4,58 @@ import Lang.Src.Compiler.Tokens
 import Lang.Src.Compiler.ExtractToken ( extractTokenInfo )
 
 data Expr = Value Token |
-            FunctionCall Token [Token] deriving (Show)
+            FunctionCall Token [Token] | 
+            EndExpr deriving (Show)
 
 data ParseTree = Assign Token Expr ParseTree |
                  Function Token [ParseTree] ParseTree |
-                 Export Expr                          |
+                 Export Expr ParseTree                |
                  EndNode deriving (Show)
 
+starParsingTree :: [Token] -> ParseTree
+starParsingTree [] = EndNode
 starParsingTree (x:xs)
     | token' == FnToken = 
-        Function fnName arguments (starParsingTree remain)
+        Function tokenIdentifier arguments (starParsingTree remain)
     | token' == IdentifierToken = 
-        Function fnName arguments (starParsingTree remain)
+        Assign x expr (starParsingTree remain')
+    | token' == ExportToken = Export expr (starParsingTree remain')
+    | otherwise = error $ show (x:xs)
     where
         token' = fst (token x)
-        fnName = head xs
+        tokenIdentifier = head xs
         (arguments, remain) = params $ tail xs
+        (expr, remain') = expressionValue xs
 
+
+params :: [Token] -> ([ParseTree], [Token])
+params [] = ([], [])
+params [x] = ([], [])
 params (x:xs) 
     | token' == OpenParamsToken = params xs
     | token' == IdentifierToken = 
         (Assign x (Value (head xs)) EndNode:ptr, remain)
-    | otherwise = ([], remain)
+    | otherwise = ([], xs)
     where
         (ptr, remain) = params $ tail xs
         token' = fst (token x)
 
-{- expressionValue (x:xs)
-    | token' == EndParamsToken = FunctionCall (head xs) expr
+expressionValue :: [Token] -> (Expr, [Token])
+expressionValue [] = (EndExpr, [])
+expressionValue [x] = (Value x, [])
+expressionValue (x:xs)
+    | token' == EndParamsToken = (FunctionCall (head xs) expr, remain)
+    | token' == IdentifierToken || token' == IntegerToken = (Value x, xs)
+    | otherwise = error $ show (x:xs)
     where
-        (expr, remain) = arguments $ tail xs
+        (expr, remain) = argumentsFnCall $ tail $ tail xs
         token' = fst (token x)
 
-arguments (x:xs)
-    | token' == IdentifierToken || token' == IntegerToken =
-        x:arguments xs
-    | token' == EndParamsToken = ([], remain)
+argumentsFnCall :: [Token] -> ([Token], [Token])
+argumentsFnCall [] = ([], [])
+argumentsFnCall (x:xs)
+    | token' == IdentifierToken || token' == IntegerToken = (x:tkList, remain)
+    | otherwise = ([], xs)
     where
-        (expr, remain) = arguments $ tail xs
-        token' = fst (token x) -}
+        token' = fst (token x)
+        (tkList, remain) = argumentsFnCall xs
