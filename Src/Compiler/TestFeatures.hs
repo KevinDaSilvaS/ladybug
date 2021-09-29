d4b5712b214e3399d7bef11535003a82f5f4f51e
Your Name <kevin.kds80@gmail.com>
2021-09-29 23:45:51 +0000

    Change test features

--- Src/Compiler/TestFeatures.hs
+++ Src/Compiler/TestFeatures.hs
@@ -3,6 +3,7 @@ module Lang.Src.Compiler.TestFeatures where
 import Lang.Src.Compiler.LexicalAnalyser
 import Lang.Src.Compiler.SintaticAnalyser
 import Lang.Src.Compiler.ParseTree
+import Lang.Src.Compiler.SemanticAnalyser
 
 tokens = do
     let program = "fn fnc <: _v=0 v1=12 :> v = 90 export 90" ---"fnw3030fn ,=..."
@@ -16,6 +17,7 @@ getAllTokensFromProgram xs l c =
         (parsedToken, line, col, remain) = startLexicalAnalysis xs l c
 
 sintaticAnalysis = do
-    let program = "r = 22 fn fnc <: _v=0 v1=12 :> v = 90 export 90 i = . fnc . 20 40 20 ."
+    let program = "r = 22 l = r fn fnc <: _v=0 v1=12 :> v = 90 export 90 i = . fnc . 20 40 20 ."
     let tokens = startSintaticAnalysis program 1 0 0 0
-    starParsingTree tokens
+    let ptr = starParsingTree tokens
+    startSemanticAnalysis ptr [] []
